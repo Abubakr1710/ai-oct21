733a0e3e48f9b70d0fc0e0fa56afbd9470b39f02
alessiorecchia <recchia.alessio@yahoo.com>
2022-01-20 15:46:07 +0100

    uploaded partial code

--- Chapter 3/99. Live_coding/03. classification_project/data_handler.py
+++ Chapter 3/99. Live_coding/03. classification_project/data_handler.py
@@ -7,8 +7,26 @@ class DataHandler:
         self.train_size = train_size
         self.root_dir = root_dir
 
-    def folder_structure(self, img_folder, dest_folders = []):
+    def folder_structure(self, img_folder, dest_folders = ['train', 'test']):
         classes = os.listdir(img_folder)
+
+
+        for item in dest_folders:
+            for i in range(len(classes)):
+                os.system(f'mkdir -p {self.root_dir}/datasets/{item}/{str(i)}')
+
+        # folder structure like this:   datasets/train/0
+        #                               datasets/train/1
+        #                               datasets/test/0
+        #                               datasets/test/1
+        #                               datasets/val/0
+        #                               datasets/val/1
+
+loader = DataHandler('/home/alessio/Desktop')
+
+loader.folder_structure(loader.root_dir + '/img')
+
+
 
     
 
--- Chapter 3/99. Live_coding/03. classification_project/test.py
+++ Chapter 3/99. Live_coding/03. classification_project/test.py
@@ -4,16 +4,29 @@ img_folder = 'img'
 
 classes = os.listdir(img_folder)
 
-paths = ['train', 'test', 'val']
+paths = ['train', 'test']
 
-# folder structure like this:   datasets/train/0
-#                               datasets/train/1
-#                               datasets/test/0
-#                               datasets/test/1
+# for f in img_folder:
+#     for p in f:
+#         print(p)
 
+# for item in paths:
+#     for i in range(len(classes)):
+#         os.system(f'mkdir -p ./datasets/{item}/{str(i)}')
 
-for item in paths:
-    for cl in classes:
-        print(f'mkdir ./datasets/{item}/{cl}')
+# os.system('mv /path/to/the/source path/to/destination')
+# 'img/person/0_7.png'
 
+# files = os.listdir(img_folder + '/' + classes[0])
 
+path_to_img = 'img'+'/'+'person'
+
+# print(path_to_img)
+
+files = os.listdir(path_to_img)
+files = sorted(files)
+print(files[1])
+
+# for item in classes:
+#     for i in range(len(item)):
+#         print(item[i])
